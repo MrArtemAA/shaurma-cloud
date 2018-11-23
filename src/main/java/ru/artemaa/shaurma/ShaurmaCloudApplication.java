70f54c0801358ae84588552dff4ec9bc872823f8
Artem Areshko <artemaa@gmail.com>
2018-11-23 09:17:04 +0300

    2.4. Simplify HomeController

--- src/main/java/ru/artemaa/shaurma/ShaurmaCloudApplication.java
+++ src/main/java/ru/artemaa/shaurma/ShaurmaCloudApplication.java
@@ -2,11 +2,18 @@ package ru.artemaa.shaurma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ShaurmaCloudApplication {
+public class ShaurmaCloudApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShaurmaCloudApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
     }
 }
--- src/test/java/ru/artemaa/shaurma/HomeControllerTest.java
+++ src/test/java/ru/artemaa/shaurma/HomeControllerTest.java
@@ -3,7 +3,6 @@ package ru.artemaa.shaurma;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
