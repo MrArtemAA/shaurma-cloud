56001da317c8cacf5388f3ea2928b39c052ba031
Artem Areshko <artemaa@gmail.com>
2018-12-10 12:27:08 +0300

    6.1.3 REST. Updating data on server

---
+++ src/main/java/ru/artemaa/shaurma/web/api/OrderRestController.java
@@ -0,0 +1,33 @@
+package ru.artemaa.shaurma.web.api;
+
+import org.springframework.web.bind.annotation.*;
+import ru.artemaa.shaurma.Order;
+import ru.artemaa.shaurma.data.jpa.OrderRepository;
+
+import static java.util.Objects.nonNull;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/orders", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+public class OrderRestController {
+    private final OrderRepository orderRepository;
+
+    public OrderRestController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @PutMapping("/{id}")
+    public Order putOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @PatchMapping("/{id}")
+    public Order patchOrder(@PathVariable("id") Long id, @RequestBody Order patchOrder) {
+        Order order = orderRepository.findById(id).get();
+        if (nonNull(patchOrder.getName())) {
+            order.setName(patchOrder.getName());
+        }
+        // etc
+        return orderRepository.save(order);
+    }
+}
