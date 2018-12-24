9ec885b1c73884d887d55f611808f261e293ad8a
Artem Areshko <artemaa@gmail.com>
2018-12-24 10:03:13 +0300

    10.3.2 Combining reactive types

--- src/test/java/ru/artemaa/shaurma/reactor/ReactorTest.java
+++ src/test/java/ru/artemaa/shaurma/reactor/ReactorTest.java
@@ -3,6 +3,7 @@ package ru.artemaa.shaurma.reactor;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -65,5 +66,74 @@ public class ReactorTest {
                 .verifyComplete();
     }
 
+    @Test
+    public void testMerge() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
 
+        Flux<String> flux = characterFlux.mergeWith(foodFlux);
+        StepVerifier.create(flux)
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testZip() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<Tuple2<String, String>> flux = Flux.zip(characterFlux, foodFlux);
+
+        StepVerifier.create(flux)
+                .expectNextMatches(pair ->
+                        pair.getT1().equals("Garfield") && pair.getT2().equals("Lasagna"))
+                .expectNextMatches(pair ->
+                        pair.getT1().equals("Kojak") && pair.getT2().equals("Lollipops"))
+                .expectNextMatches(pair ->
+                        pair.getT1().equals("Barbossa") && pair.getT2().equals("Apples"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void testZipFunction() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        Flux<String> flux = Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+        StepVerifier.create(flux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testFirst() {
+        Flux<String> slowFlux = Flux
+                .just("tortoise", "snail", "sloth")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> fastFlux = Flux
+                .just("hare", "cheetah", "squirrel");
+
+        Flux<String> flux = Flux.first(slowFlux, fastFlux);
+        StepVerifier.create(flux)
+                .expectNext("hare")
+                .expectNext("cheetah")
+                .expectNext("squirrel")
+                .verifyComplete();
+    }
 }
