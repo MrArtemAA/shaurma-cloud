a01fa09af076e02e6b51a7558b73a674dd22857e
Artem Areshko <artem.areshko@mvideo.ru>
2018-12-25 10:24:44 +0300

    10.3.3 Transforming and filtering reactive streams

--- src/test/java/ru/artemaa/shaurma/reactor/ReactorTest.java
+++ src/test/java/ru/artemaa/shaurma/reactor/ReactorTest.java
@@ -1,12 +1,17 @@
 package ru.artemaa.shaurma.reactor;
 
+import lombok.Data;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Arrays.asList;
 
@@ -134,6 +139,155 @@ public class ReactorTest {
                 .expectNext("hare")
                 .expectNext("cheetah")
                 .expectNext("squirrel")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testSkip() {
+        Flux<String> flux = Flux.just("one", "two", "three", "four", "five")
+                .skip(3);
+
+        StepVerifier.create(flux)
+                .expectNext("four", "five")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testSkipDuration() {
+        Flux<String> flux = Flux.just("one", "two", "three", "four", "five")
+                .delayElements(Duration.ofSeconds(1))
+                .skip(Duration.ofSeconds(4));
+
+        StepVerifier.create(flux)
+                .expectNext("four", "five")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testTake() {
+        Flux<String> flux = Flux.just("one", "two", "three", "four", "five")
+                .take(3);
+
+        StepVerifier.create(flux)
+                .expectNext("one", "two", "three")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testTakeDuration() {
+        Flux<String> flux = Flux.just("one", "two", "three", "four", "five")
+                .delayElements(Duration.ofSeconds(1))
+                .take(Duration.ofMillis(3500));
+
+        StepVerifier.create(flux)
+                .expectNext("one", "two", "three")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testFilter() {
+        Flux<String> flux = Flux.just("one", "two", "twenty three", "forty four", "five")
+                .filter(s -> !s.contains(" "));
+
+        StepVerifier.create(flux)
+                .expectNext("one", "two", "five")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testDistinct() {
+        Flux<String> flux = Flux.just("one", "two", "three", "one", "four", "five", "two")
+                .distinct();
+
+        StepVerifier.create(flux)
+                .expectNext("one", "two", "three", "four", "five")
+                .verifyComplete();
+    }
+
+    @Data
+    private class Player {
+        private final String firstName;
+        private final String lastName;
+    }
+
+    @Test
+    public void testMap() {
+        Flux<Player> flux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                .map(name -> {
+                    String[] split = name.split(" ");
+                    return new Player(split[0], split[1]);
+                });
+
+        StepVerifier.create(flux)
+                .expectNext(new Player("Michael", "Jordan"))
+                .expectNext(new Player("Scottie", "Pippen"))
+                .expectNext(new Player("Steve", "Kerr"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void testFlatMap() {
+        Flux<Player> flux = Flux
+                .just("Michael Jordan", "Scottie Pippen", "Steve Kerr")
+                .flatMap(s -> Mono.just(s)
+                        .map(name -> {
+                            String[] split = name.split(" ");
+                            return new Player(split[0], split[1]);
+                        })
+                        .subscribeOn(Schedulers.parallel())
+                        .log()
+                );
+
+        List<Player> players = asList(
+                new Player("Michael", "Jordan"),
+                new Player("Scottie", "Pippen"),
+                new Player("Steve", "Kerr")
+        );
+
+        StepVerifier.create(flux)
+                .expectNextMatches(players::contains)
+                .expectNextMatches(players::contains)
+                .expectNextMatches(players::contains)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testBuffer() {
+        Flux<List<String>> flux = Flux.fromArray(fruits)
+                .buffer(3);
+
+        StepVerifier.create(flux)
+                .expectNext(Arrays.asList(fruits[0], fruits[1], fruits[2]))
+                .expectNext(Arrays.asList(fruits[3], fruits[4]))
+                .verifyComplete();
+
+        Flux.fromArray(fruits)
+                .buffer(3)
+                .flatMap(fruitList -> Flux.fromIterable(fruitList)
+                        .map(String::toUpperCase)
+                        .subscribeOn(Schedulers.parallel())
+                        .log()
+                ).subscribe();
+    }
+
+    @Test
+    public void testCollectList() {
+        Mono<List<String>> collectList = Flux.fromArray(fruits)
+                .collectList();
+
+        StepVerifier.create(collectList)
+                .expectNext(asList(fruits))
+                .verifyComplete();
+    }
+
+    @Test
+    public void testCollectMap() {
+        Mono<Map<Character, String>> collectMap = Flux.fromArray(fruits)
+                .collectMap(elem -> elem.charAt(0));
+
+        StepVerifier.create(collectMap)
+                .expectNextMatches(map -> map.size() == 5)
                 .verifyComplete();
     }
 }
